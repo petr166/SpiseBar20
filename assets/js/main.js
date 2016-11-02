77453ba9d115b242ed20cb569a2d70ac5d894899
Petru Birzu <petru.birzu96@gmail.com>
2016-11-02 23:18:04 +0100

    add scroll animation to section jumping

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,21 @@
 $(function() {
-    $('body').removeClass('fade-out'); //fade-in the body
-    $('#main_title').fadeIn(2500); //fade-in the title(slow)
+  showContent();
+  smoothScroll();
 });
+
+//fade in the home page
+function showContent() {
+  $('body').removeClass('fade-out'); //fade-in the body
+  $('#main_title').fadeIn(2500); //fade-in the title(slow)
+}
+
+//add smooth scroll effect when jumping to sections
+function smoothScroll() {
+  var $root = $('html, body');
+  $('a').click(function(){
+    $root.animate({
+        scrollTop: $( $(this).attr('href') ).offset().top
+    }, 500);
+    return false;
+  });
+}
