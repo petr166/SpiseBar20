79b3f18b445e78bdeea362ebd56101c3f2d81a0b
Petru Birzu <petru.birzu96@gmail.com>
2016-11-05 16:53:52 +0100

    ajust #main_title fade in animation

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,8 +5,8 @@ window.onload = function() {
 
 //fade in the home page
 function showContent() {
-  $('body').removeClass('fade-out'); //fade-in the body
-  $('#main_title').fadeIn(3000); //fade-in the title(slow)
+  $('#page_container').removeClass('fade-out'); //fade-in the body
+  setTimeout(function() {$('#main_title').removeClass('fade-out');}, 500); //fade-in the title(slow)
 }
 
 //add smooth scroll effect when jumping to sections
