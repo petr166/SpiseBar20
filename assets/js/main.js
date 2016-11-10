94932e11acb7b563e7b2eb855b0faa17dd9af75a
Dana Iliescu <dana.iliescu96@gmail.com>
2016-11-10 10:28:11 +0100

    wine section work

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,7 @@
 $(function() {
   showContent();
   smoothScroll();
+  loadImages();
 });
 
 //fade in the home page
@@ -18,4 +19,22 @@ function smoothScroll() {
     }, 800);
     return false;
   });
+}
+
+function loadImages() {
+  var folder = "img/carousel/";
+  $.ajax({
+    url : folder,
+    success: function (data) {
+      $(data).find("a").attr("href", function (i, val) {
+        if( val.match(/\.(jpe?g|png|gif)$/) ) {
+          $("<div class='item'><img src='"+ folder + val +"'></div>").appendTo('.carousel-inner');
+          $('<li data-target="#carousel" data-slide-to="'+ 0 +'"></li>').appendTo('.carousel-indicators')
+        }
+      });
+    }
+  });
+  $('.item').first().addClass('active');
+  $('.carousel-indicators > li').first().addClass('active');
+  $('#carousel').carousel();
 }
