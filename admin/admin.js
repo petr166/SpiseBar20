3bc5d23bed964e841d8ef7113a009c0f0c801ac9
Dana Iliescu <dana.iliescu96@gmail.com>
2016-11-19 00:14:58 +0100

    menu items displayed dynamically

--- admin/admin.js
+++ admin/admin.js
@@ -12,6 +12,30 @@ function getMenu() {
     'success' : function(dataIN) {
       var jsonObj = JSON.parse(dataIN);
       console.log(jsonObj.data);
+      displayMenu(jsonObj);
     }
+  });
+}
+
+//function to display the menu from the jsonObj
+function displayMenu(jsonObj) {
+  $.each(jsonObj.data, function(index, category) {
+    console.log(category);
+    $('.container-fluid').append("<div class='row editable' id='" + category.id + "'>" +
+                                  "<center><h4 class='food-type'>" + category.name +
+                                          "</h4></center></div>");
+
+    $.each(category.items, function(index, value) {
+      console.log(value);
+      $('.container-fluid').append("<div class='row food-item' id='" + value.id + "'>" +
+                                    "<div class='row'>" +
+                                      "<h4 class='col-md-6 food-name editable'>" + value.title + "</h4>" +
+                                      "<span class='col-md-6 food-price editable'>" + value.price + "</span>" +
+                                    "</div>" +
+                                    "<div class='row'>" +
+                                      "<p class='col-md-6 food-description editable'>" + value.description + "</p>" +
+                                    "</div>" +
+                                  "</div>");
+    });
   });
 }
